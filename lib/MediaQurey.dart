2a0d70ea1b2405f38a7f4a7b640944ab9f40210e
Partha Majumder <parthamajumder9810@gmail.com>
2025-07-05 23:07:32 +0530

    Media Query flutter basic code

--- lib/MediaQurey.dart
+++ lib/MediaQurey.dart
@@ -1,18 +1,36 @@
-// import 'package:flutter/material.dart';
-//
-// void main () => runApp(MyApp());
-//
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//
-//     );
-//   }
-// }
+import 'package:flutter/material.dart';
 
-void main () {
-  print('Hello world!');
+void main () => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage()
+
+    );
+  }
 }
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+          body: Center(
+            child: Container(
+              height: MediaQuery.of(context).size.height / 4,
+              width: MediaQuery.of(context).size.width / 3,
+              color: Colors.blue,
+            ),
+          ),
+        )
+    );
+  }
+}
+
+
