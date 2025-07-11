a1e165e1d53885824dad708fba69a6d045dfffec
Partha Majumder <parthamajumder9810@gmail.com>
2025-07-11 23:36:36 +0530

    new dart code

--- lib/practise.dart
+++ lib/practise.dart
@@ -3,15 +3,39 @@ import 'dart:io';
 void main () {
   print('Hello, Dart program');
 
-  stdout.write('Enter your name:');
+  // stdout.write('Enter your name:');
 
-  var name = stdin.readLineSync();
+  // var name = stdin.readLineSync();
+  //
+  // print("Welcome, $name");
 
-  print("Welcome, $name");
+  // Home(); // #create an new class object
 
-  Home(); // create an new class object
+  // * Number Variables
+
+  //Declaration of variable
+  // int ? a;
+  // a = 5; // initialization
+  //
+  // print(a);
+  //
+  // a = 7;
+  // print(a);
+
+  BigInt longValue;
+  longValue = BigInt.parse('886853689783758659999999999999999999999999999998560708789586');
+
+  print(longValue);
+
+  double point = 88.04;
+
+  print(point);
+
+  // inline declaration
+
+
 }
 
-class Home {
-
-}
+// class Home {
+//
+// }
---
+++ lib/practise2.dart
@@ -0,0 +1,19 @@
+
+
+void main () {
+  print("Wel-come to Dart");
+
+  String name = "RAM";
+
+  // Var and Dynamic
+  // Var
+
+  var Love;
+
+  Love = 'Ram';
+  print(Love);
+
+  Love = 20;
+
+  print(Love);
+}
