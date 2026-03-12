a17d3d33854109808fe173fcab634d64e5e8448d
Partha Majumder <parthamajumder9810@gmail.com>
2026-03-12 17:05:26 +0530

    Circle avater in flutter

--- lib/CircleAvatar.dart
+++ lib/CircleAvatar.dart
@@ -1,4 +1,3 @@
-import 'package:application_demo1/appBar.dart';
 import 'package:flutter/material.dart';
 
 void main () {
@@ -11,45 +10,40 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Circle Avatar",
+      title: 'Circle Avatar Page',
       theme: ThemeData(
-        primarySwatch: Colors.lightGreen
+        primaryColor: Colors.green
       ),
-      home: CircleAvatarScreen(),
+      home: HomepageScreen(),
     );
   }
 }
 
-class CircleAvatarScreen extends StatelessWidget{
-  const CircleAvatarScreen({super.key});
+class HomepageScreen extends StatelessWidget {
+  const HomepageScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-
-    var arrList = ['One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten'];
-
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text("Circle Avatar", style: TextStyle(fontFamily: 'serif'),)),
-        backgroundColor: Colors.lightGreen,
+        title: Center(child: Text('Circle Avatar page')),
       ),
-      body: ListView.separated(itemBuilder: (context, index){
-      return ListTile(
-        leading: Text('${index+1}'),
-        title: Text(arrList[index]),
-        subtitle: Text("Give the number list", style: TextStyle(fontFamily: 'serif', fontSize: 15),),
-        trailing: Icon(Icons.add),
-      );
-      },
-      itemCount: arrList.length,
-        separatorBuilder: (context, index){
-        return Divider(
-          height: 20,
-          thickness: 1,
-        );
-        },
+      body: Center(
+          child: CircleAvatar(
+            backgroundColor: Colors.green,
+            maxRadius: 40,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 40,
+                    width: 40,
+                    child: Image.asset('images/me.png')),
+                Text('Text', style: TextStyle(color: Colors.red),)
+              ],
+            ),
+          ),
       ),
     );
   }
 }
+
